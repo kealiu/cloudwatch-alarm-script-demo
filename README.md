9380b60d1f3a6f209b0d262b8e4220bc99158f7d
Ubuntu <ubuntu@ip-172-31-40-99.ap-northeast-1.compute.internal>
2022-03-29 03:03:35 +0000

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# cloudwatch-alarm-script-demo
-create cloudwatch alarm in batch mode
+# Create Alarm Manually
+
+as for ELB, target group metric, as the following steps
+
+![cloudwatch-all-metrics](https://github.com/kealiu/cloudwatch-alarm-script-demo/raw/main/images/cloudwatch-all-metrics.png)
+![cloudwatch-elb-metrics](https://github.com/kealiu/cloudwatch-alarm-script-demo/raw/main/images/cloudwatch-elb-metrics.png)
+![cloudwatch-target-metrics](https://github.com/kealiu/cloudwatch-alarm-script-demo/raw/main/images/cloudwatch-target-metrics.png)
+![cloudwatch-graph-metrics](https://github.com/kealiu/cloudwatch-alarm-script-demo/raw/main/images/cloudwatch-graph-metrics.png)
+![cloudwatch-metric-alarm](https://github.com/kealiu/cloudwatch-alarm-script-demo/raw/main/images/cloudwatch-metric-alarm.png)
+![cloudwatch-alarm-create](https://github.com/kealiu/cloudwatch-alarm-script-demo/raw/main/images/cloudwatch-alarm-create.png)
+
+# Create Alarm Script
+
+see [sample code](https://github.com/kealiu/cloudwatch-alarm-script-demo/blob/main/batch-alarm.sh)
--- batch-alarm.sh
+++ batch-alarm.sh
@@ -3,13 +3,16 @@
 METRIC_NAME="HTTPCode_Target_4XX_Count"
 ALARM_PERIOD=60
 ALARM_THRESHOLD=10
+ALARM_COUNTS=1
 ALARM_SNS_ARN=arn:aws:sns:us-east-1:111122223333:MyTopic
 
 # get interested metrics
-
+# refer: https://awscli.amazonaws.com/v2/documentation/api/latest/reference/cloudwatch/list-metrics.html
 TARGETS=$(aws cloudwatch list-metrics --namespace "AWS/ApplicationELB" --metric-name ${METRIC_NAME} | jq -r '.Metrics[].Dimensions[] | select(.Name=="TargetGroup") | .Value')
 
+# create alarms for all target group
+# refer: https://awscli.amazonaws.com/v2/documentation/api/latest/reference/cloudwatch/put-metric-alarm.html
 for target in ${TARGETS}
 do
-	aws cloudwatch put-metric-alarm --alarm-name "ELB-ALARM-TargetGroup-${target}" --alarm-description "Alarm for target group ${target}" --metric-name ${METRIC_NAME} --namespace AWS/ApplicationELB --statistic Sum --period ${ALARM_PERIOD} --threshold ${ALARM_THRESHOLD} --comparison-operator GreaterThanThreshold  --dimensions "Name=TargetGroup,Value=${target}"  --evaluation-periods 1 --alarm-actions ${ALARM_SNS_ARN} --unit Count
+	aws cloudwatch put-metric-alarm --alarm-name "ELB-ALARM-TargetGroup-${target}" --alarm-description "Alarm for target group ${target}" --metric-name ${METRIC_NAME} --namespace AWS/ApplicationELB --statistic Sum --period ${ALARM_PERIOD} --threshold ${ALARM_THRESHOLD} --comparison-operator GreaterThanThreshold  --dimensions "Name=TargetGroup,Value=${target}"  --evaluation-periods ${ALARM_COUNTS} --alarm-actions ${ALARM_SNS_ARN} --unit Count
 done
